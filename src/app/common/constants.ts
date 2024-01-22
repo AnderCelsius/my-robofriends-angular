14103203e875d65e3f13314845b82a18e28ba469
Obinna Anderson Asiegbulam <oasiegbulam@gmail.com>
2024-01-22 02:27:46 +0100

    update template

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,7 +1,15 @@
-export const ROBOT_SETS = {
+export const ROBOT_SETS_VALUE = {
   REGULAR: 'Regular',
   MONSTER: 'Monster',
   DISEMBODIED: 'Disembodied',
   KITTEN: 'Kitten',
   HUMAN: 'Human',
 };
+
+export const ROBOT_SETS = {
+    SET1: 'set1',
+    SET2: 'set2',
+    SET3: 'set3',
+    SET4: 'set4',
+    SET5: 'set5',
+  };
