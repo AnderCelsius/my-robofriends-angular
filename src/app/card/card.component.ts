ce11a02f24ad1968b35d93154ea4d7229411a1dd
Obinna Anderson Asiegbulam <oasiegbulam@gmail.com>
2024-01-22 02:47:34 +0100

    update template

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { IRobot } from '../model/robot';
 import "tachyons";
-import {  NgIf } from '@angular/common';
+import {  NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { ROBOT_SETS, ROBOT_SETS_VALUE } from '../common/constants';
 
 // Use NgClass when you want to dynamically add styles to template
 
 @Component({
   selector: 'robot-card',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgSwitch, NgSwitchCase],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -22,6 +23,9 @@ export class CardComponent {
     required: true,
   })
   isEven: boolean = false;
+
+  robotSets = ROBOT_SETS;
+  ROBOT_TYPE = ROBOT_SETS_VALUE;
 
   constructor() {
     this.robot = {
@@ -48,7 +52,6 @@ export class CardComponent {
   }
 
   cardClasses() {
-
   }
 }
 
